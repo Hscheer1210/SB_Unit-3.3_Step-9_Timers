86dc4527f83d72224dd397b55badd7fd55c2bed3
Hscheer1210 <82344547+Hscheer1210@users.noreply.github.com>
2021-09-24 14:22:20 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+function countdown(x) {
+    let secondsRemaining = x;
+    let secondsInterval = setInterval(function(){
+        if(secondsRemaining <= 0){
+            clearInterval(secondsInterval);
+            console.log("DONE!");
+        } else {
+            console.log(secondsRemaining);
+        }
+        secondsRemaining -= 1;
+    }, 1000);
+}
+
+function randomGameOne() {
+    let attemptNumber = 1;
+    let randomInterval = setInterval(function(){
+        if(Math.random() >= 0.75){
+            clearInterval(randomInterval);
+            console.log("Number of tries it took: " + (attemptNumber - 1));
+        } else {
+            console.log("Attempt #" + attemptNumber, Math.random());
+        }
+        attemptNumber += 1;
+    }, 1000);
+}
+
+function randomGameTwo() {
+    let attemptNumber = 1;
+    let randomNumber = Math.random();
+    let randomInterval = setInterval(function(){
+        if(randomNumber >= 0.75){
+            clearInterval(randomInterval);
+            console.log("Number of tries it took: " + (attemptNumber - 1));
+        } else {
+            console.log("Attempt #" + attemptNumber, randomNumber);
+        }
+        attemptNumber += 1;
+    }, 1000);
+}
