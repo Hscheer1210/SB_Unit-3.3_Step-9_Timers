9dbf48e1c71add6209eb6e5ff7c1aec59fb72fb9
Hscheer1210 <82344547+Hscheer1210@users.noreply.github.com>
2021-09-24 14:43:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ function countdown(x) {
     }, 1000);
 }
 
+// Note: randomGameV1 (version #1) returns values greater than 0.75 
+// I assume it's because the Math.random() function is running two separate times, both independent of each other
+
 function randomGameV1() {
     let attemptNumber = 1;
     let randomInterval = setInterval(function(){
@@ -25,6 +28,9 @@ function randomGameV1() {
         attemptNumber += 1;
     }, 1000);
 }
+
+// Note: randomGameV2 (version #2) returns the same random value over and over
+// I assume it's because I have the Math.random() function stored in a variable
 
 function randomGameV2() {
     let attemptNumber = 1;
